0050a8fa3cf4d73b73766f16c678960fde78cf6f
Garrett Baker <garrett_baker2002@yahoo.com>
2017-06-26 10:13:15 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+var cors = require('cors');
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/:dateVal", function (req, res, next){
+  var dateVal = req.params.dateVal;
+
+  var dateFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  if(isNaN(dateVal)){
+     var naturalDate = new Date(dateVal);
+      naturalDate = naturalDate.toLocaleDateString("en-us", dateFormatOptions);
+    var unixDate = new Date(dateVal).getTime()/1000;
+  } else {
+    var unixDate = dateVal;
+    var naturalDate = new Date(dateVal * 1000);
+  }
+  res.json({unix: unixDate, natural : naturalDate});
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on ' + listener.address().port););
+});
